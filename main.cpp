7dcb0f44c9c908406df39523fa69db9d899ab0c6
frongerio <ervimoz05@gmail.com>
2026-06-25 09:47:36 +0300

    main.cpp pridejimas

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "[order-sentinel] starting up...\n";
+    std::cout << "  target  : wss://stream.binance.com:9443\n";
+    std::cout << "  symbol  : BTCUSDT\n";
+    return 0;
+}
